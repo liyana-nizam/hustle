4b681e2bc366c1b36f82611eb568002af678ab50
Syuhada279 <d032410279@student.utem.edu.my>
2026-06-09 09:59:45 +0800

    Modify footer

--- gigworkerRate.php
+++ gigworkerRate.php
@@ -10,60 +10,61 @@
 
 <body>
     <?php include('header-owner.php') ?>
-    
+
     <div class="gigworkerRate-container">
 
-    <div class="back-btn">
-        <a href="progress-owner.php">
-            <img src="images/back.png" alt="Back" class="icon">
-            Rate Gig Worker
-        </a>
+        <div class="back-btn">
+            <a href="progress-owner.php">
+                <img src="images/back.png" alt="Back" class="icon">
+                Rate Gig Worker
+            </a>
 
 
-        <div class="item-container">
-            <div class="gig-left">
-                <div class="gig-img"><img src="images/cleaning.png"></div>
-                <div class="gig-info">
-                    <p class="gig-name">Need Help with Laundry</p>
-                    <p class="gig-salary">RM 20</p>
+            <div class="item-container">
+                <div class="gig-left">
+                    <div class="gig-img"><img src="images/cleaning.png"></div>
+                    <div class="gig-info">
+                        <p class="gig-name">Need Help with Laundry</p>
+                        <p class="gig-salary">RM 20</p>
+                    </div>
+                </div>
+
+                <div class="gig-right">
+                    <button class="gig-filter">Cleaning</button>
+                    <button class="gig-filter">Melaka Tengah</button>
                 </div>
             </div>
 
-            <div class="gig-right">
-                <button class="gig-filter">Cleaning</button>
-                <button class="gig-filter">Melaka Tengah</button>
+            <h2>Gig Completion Proof</h2>
+            <ul id="fileList">
+                <li> Job Title.jpg <button class="view-btn">View</button></li>
+            </ul>
+
+            <hr class="divider">
+
+            <div class="rating-container">
+                <h3>Rate your Gig Worker</h3>
+                <div class="formSection">
+                    <input type="number" id="rateInput" required placeholder="1 - 5" min="1" max="5">
+                </div>
             </div>
-        </div>
 
-        <h2>Gig Completion Proof</h2>
-        <ul id="fileList">
-            <li> Job Title.jpg <button class="view-btn">View</button></li>
-        </ul>
-
-        <hr class="divider">
-
-        <div class="rating-container">
-            <h3>Rate your Gig Worker</h3>
-            <div class="formSection">
-                <input type="number" id="rateInput" required placeholder="1 - 5" min="1" max="5">
+            <div class="rating-container">
+                <h3>Give a Feedback Message</h3>
+                <div class="formSection">
+                    <textarea name="feedbackMessage" id="feedbackMessage" rows="4" placeholder="Type your message here..."></textarea>
+                </div>
             </div>
-        </div>
 
-        <div class="rating-container">
-            <h3>Give a Feedback Message</h3>
-            <div class="formSection">
-                <textarea name="feedbackMessage" id="feedbackMessage" rows="4" placeholder="Type your message here..."></textarea>
+            <h2>Proof: Upload Payment Receipt</h2>
+            <div class="file-upload-container">
+                <button class="choose-btn">Choose Files</button>
             </div>
+
+            <button class="submit-btn">Submit</button>
         </div>
 
-        <h2>Proof: Upload Payment Receipt</h2>
-        <div class="file-upload-container">
-            <button class="choose-btn">Choose Files</button>
-        </div>
-
-        <button class="submit-btn">Submit</button>
     </div>
-
     <footer>
         <?php include('footer-owner.php') ?>
     </footer>
